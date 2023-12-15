373e11c30d5cb3360b5fec26899a32c4c4404a66
Md Mahbub Alom <mdmahbuba034@gmail.com>
2023-12-15 20:10:52 +0600

    some work done

---
+++ app.js
@@ -0,0 +1,34 @@
+var firstName=true;
+// console.log(typeof firstName);
+
+
+var num1=10;
+var num2="20";
+var parseResult = parseInt(num2)
+// console.log(num1 * num2)
+
+
+var name = 'ablu';
+if(name = 'hablu'){
+    // console.log("this is hablu")
+}
+else if(name = "kablu"){
+    // console.log("this is kablu")
+}
+else{
+    // console.log("suiiiiii")
+}
+
+var number = "300";
+ 
+if(number==30){
+    if(typeof number == "string"){
+        console.log("this is 30 also a string")
+    }
+    else{
+        console.log("this is 30 but not string")
+    }
+}
+else{
+    console.log("this is not 30")
+}
